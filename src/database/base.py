da90654b65282aed19cce17d88d4573eca0c1244
Diego Silva <diego23montes@outlook.com>
2025-08-30 18:28:08 -0600

    upd:  string donde se explica el codigo

--- src/database/base.py
+++ src/database/base.py
@@ -1,5 +1,12 @@
 from sqlalchemy.orm import DeclarativeBase
 
+
+"""
+clase la cual heredan todos los modelos en 
+las distintas logicas del negocio o microservicio
+"""
+
+# exportamos a otros módulos (modelos)
 class Base(DeclarativeBase):
     """Base común para todos los modelos."""
     pass
