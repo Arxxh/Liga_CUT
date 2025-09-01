a9ad55840add6ce750b60dc173e3fb15d0117c63
Diego Silva <diego23montes@outlook.com>
2025-08-31 20:16:22 -0600

    upd: definiendo esquemas de un usuario tanto en login y register, permisos, token

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -1,7 +1,29 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field
 
-class user(BaseModel):
-    username: str
-    password: str
+# Autenticacion
+class UserCreate(BaseModel): # POST auth/register
     email: str
+    password: str = Field(min_length=6, max_length=128)
+
+class UserLogin(BaseModel): # POST auth/login
+    email: str
+    password: str
+
+# token bearer JWT
+class Token(BaseModel): 
+    access_token: str
+    token_type: str = "bearer"
+
+# Usuario
+class UserRead(BaseModel):
+    id: int
+    email: EmailStr
+    is_active: bool
+
+    class Config:
+        from_attributes = True  # Pydantic v2 (antes: orm_mode=True)
+
+
+# Permisos
+
 
