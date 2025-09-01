8a0b6177a523a9fb24e0a8aec2ddaa1159d0066f
Diego Silva <diego23montes@outlook.com>
2025-08-31 20:17:32 -0600

    add: inicializando el archivo de service donde haremos el trabajo de encodificacion .encode() y decodificacion .decode() , jwt entre otras cosas

--- src/auth/service.py
+++ src/auth/service.py
@@ -1,5 +1,11 @@
 """
 aqui va todo el codigo de la autenticacion, login, registro, etc.
 jwt, oauth2, etc.
-SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES ETC...
+SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES ETC... 
+utilizaremos .env donde estan las variables de entorno 
 """
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Cargar variables de entorno desde el archivo .env
