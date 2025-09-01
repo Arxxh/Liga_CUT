801738c77fdb58019c086fbc0af8a42aed57d8ea
Diego Silva <diego23montes@outlook.com>
2025-09-01 09:11:13 -0600

    Add: archivo excepcion para manejar errores en mi API

---
+++ src/exceptions.py
@@ -0,0 +1,8 @@
+from fastapi import HTTPException, status
+
+"""
+Fabricaremos excepciones personalizadas para manejar errores comunes
+por ejemplo de autenticacion, autorizacion y validacion de datos, haremos 
+tambien excepciones para manejar errores de base de datos y otros errores comunes.
+"""
+
