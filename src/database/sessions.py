b85fd3f0962132c9733b2befc52e18769ce0e3cc
Diego Silva <diego23montes@outlook.com>
2025-08-31 20:18:44 -0600

    upd: ahora la base de datos es asincrona, mi dependencia se convirtio asincrona

--- src/database/sessions.py
+++ src/database/sessions.py
@@ -1,28 +1,27 @@
 # Liga_CUT/src/database/session.py
 import os
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+
 
 # Carga variables del .env (está en la raíz del microservicio)
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL_ASYNC")
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL no está definida en el .env")
 
-engine = create_engine(DATABASE_URL, future=True)
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+engine = create_async_engine(DATABASE_URL, future=True, echo=False)
+SessionLocal = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
 
 # Dependencia para FastAPI
-def get_db():
+async def get_db():
     """Dependency para FastAPI. 
     podemos exportar esta función y usarla en los endpoints como una dependiencia
     para tener acceso a la sesión de la base de datos.
+
+    Nueva dependencia que se convierte en una función asíncrona.
     """
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+    async with SessionLocal() as session:
+        yield session
