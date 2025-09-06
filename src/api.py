e19b0572fff8b502047c159d0427ef017284de60
Diego Silva <diego23montes@outlook.com>
2025-09-06 13:03:23 -0600

    add: inscripcion de mi ruta (panel) a la funcion que despues esta funciona la importamos a main

--- src/api.py
+++ src/api.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from auth.controller import router as auth_router
+from panel.controller import router as panel_router
 
 
 def register_routers(app: FastAPI):
     app.include_router(auth_router)
+    app.include_router(panel_router)
