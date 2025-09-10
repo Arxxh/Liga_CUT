5de3252926182ba463f0feef5bb668112c54b2c3
Diego Silva <diego23montes@outlook.com>
2025-09-09 19:51:46 -0600

    update: hacemos un manejo de errores en base a lo creado, email ya creado(409), contraseña debil( Unproccesable entity 422), email o contraseña incorrecta (401) y algunos errores no funcionales como el 403 forbidden y not found 404

--- src/exceptions.py
+++ src/exceptions.py
@@ -6,3 +6,25 @@ por ejemplo de autenticacion, autorizacion y validacion de datos, haremos
 tambien excepciones para manejar errores de base de datos y otros errores comunes.
 """
 
+# exceptions.py
+from fastapi import HTTPException, status
+
+class UserAlreadyExistsError(HTTPException):
+    def __init__(self):
+        super().__init__(status_code=status.HTTP_409_CONFLICT, detail="Email already registered")
+
+class InvalidCredentialsError(HTTPException):
+    def __init__(self):
+        super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid email or password")
+
+class WeakPasswordError(HTTPException):
+    def __init__(self, msg: str = "Weak password"):
+        super().__init__(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=msg)
+
+class ForbiddenError(HTTPException):
+    def __init__(self, msg: str = "Forbidden"):
+        super().__init__(status_code=status.HTTP_403_FORBIDDEN, detail=msg)
+
+class NotFoundError(HTTPException):
+    def __init__(self, msg: str = "Not found"):
+        super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail=msg)
