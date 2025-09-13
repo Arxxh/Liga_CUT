da7b4fb0dd14902b7d66ae9bd026ae041ce46cfc
Diego Silva <diego23montes@outlook.com>
2025-09-12 18:46:17 -0600

    add: exportacion de RegisterPage() para la page de register

---
+++ web/src/app/auth/register/page.tsx
@@ -0,0 +1,9 @@
+import RegisterForm from "./RegisterForm";
+
+export default function RegisterPage() {
+    return (
+        <main className="min-h-screen grid place-items-center p-6">
+            <RegisterForm />
+        </main>
+    );
+}
